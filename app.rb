c5173f592a7998116e8354f56f29529e6441c00f
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-04-06 21:31:41 +0000

    complete

--- app.rb
+++ app.rb
@@ -2,8 +2,38 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:main_page)
+  
 end
+
+
+get("/rock") do
+  @our_action = "We played rock!"
+  
+  @computer_decision = ["rock", "paper", "scissors"].sample
+  
+  @random_action = "They played " + @computer_decision.to_s + "!"
+
+  erb(:rock)
+end 
+
+get("/paper") do
+  @our_action = "We played paper!"
+
+  @computer_decision = ["rock", "paper", "scissors"].sample
+  
+  @random_action = "They played " + @computer_decision.to_s + "!"
+
+  erb(:paper)
+end 
+
+get("/scissors") do
+  @our_action = "We played scissors!"
+
+  @computer_decision = ["rock", "paper", "scissors"].sample
+  
+  @random_action = "They played " + @computer_decision.to_s + "!"
+
+  erb(:scissors)
+end 
